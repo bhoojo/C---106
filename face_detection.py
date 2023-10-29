13301b0a4e77c0b9252d2291584d2ecfb028af49
bhoojo <126891231+bhoojo@users.noreply.github.com>
2023-10-29 10:20:26 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,18 @@
+import cv2
+
+img = cv2.imread("boy.jpg")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+for(x, y, w, h) in face:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+
+    roi_color = img[y: y+h, x: x+w]
+
+    cv2.imwrite("face.jpg", roi_color)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
